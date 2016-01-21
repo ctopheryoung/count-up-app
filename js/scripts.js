43a2364d16232a368ca005496d5c32d13a312a0a
Chris Young and Ramon de la Cruz <ctopheryoung@gmail.com>
2016-01-21 11:43:32 -0800

    finish specs, pass all specs, wrote UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
-var countUp = function(input) {
+var countUp = function(countTo, countBy) {
   var output = [];
-  for (var n=1; n <= input ; n++) {
+  for (var n=countBy; n <= countTo ; n+= countBy) {
     output.push(n);
   };
-  console.log(output);
   return output;
 };
+
+
+$(document).ready(function() {
+  $("form#numbers").submit(function(event) {
+    var countTo = parseInt($("input#countTo").val());
+    var countBy = parseInt($("input#countBy").val());
+    var endResult = countUp (countTo,countBy);
+
+    $("#results").text(endResult);
+
+
+
+    event.preventDefault();
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,14 @@
 describe("countUp", function() {
-  it("counts up to the number inputed by the user", function() {
-    expect(countUp(6)).to.equal[1, 2, 3, 4, 5, 6]
+  it("counts up 1 to the number 5", function() {
+    expect(countUp(5, 1)).to.eql([1, 2, 3, 4, 5])
   });
 
-  it("counts up to the first number input by the user by the second number input by the user", function() {
-    expect(countUp(10, 2)).to.equal[2, 4, 6, 8, 10]
+  it("counts up to 10 by 2", function() {
+    expect(countUp(10, 2)).to.eql([2, 4, 6, 8, 10])
   });
+
+  it("counts up to 18 by 3", function() {
+    expect(countUp(18, 3)).to.eql([3, 6, 9, 12, 15, 18])
+  });
+
 });

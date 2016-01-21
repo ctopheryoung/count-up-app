80896e4c1086bf59a4d65bbbf59b2f3173046777
Chris Young and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-21 13:46:07 -0800

    added logic to print output array into unordered list

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,43 @@ var countUp = function(countTo, countBy) {
   return output;
 };
 
+function write_letters(endResult){
+    var items = document.getElementById("itemList");
+    for (var i = 0; i < endResult.length; i++ ) {
+        var item = document.createElement("li");
+        item.innerHTML = endResult[i];
+        items.appendChild(item);
+    };
+    return;
+};
+
+
+
 
 $(document).ready(function() {
-  $("form#numbers").submit(function(event) {
+  $("form#numbers").submit(function() {
+    //debugger;
     var countTo = parseInt($("input#countTo").val());
     var countBy = parseInt($("input#countBy").val());
     var endResult = countUp (countTo,countBy);
+    write_letters(endResult);
 
-    $("#results").text(endResult);
+    // write_letters(endResult) {
+    //   return items;
+    // }
 
-
+    //$("#itemList").text(endResult);
 
     event.preventDefault();
-
   });
 });
+
+
+
+// var countUp = function(countTo, countBy) {
+//   var output = [];
+//   for (var n=countBy; n <= countTo ; n+= countBy) {
+//     output.push(n);
+//   };
+//   return output;
+// };
